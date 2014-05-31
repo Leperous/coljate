219420b4fcf94a65a832fbd46f570f5ab007687d
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2014-05-31 12:16:39 +0100

    Queue interface

---
+++ src/main/java/net/ollie/sc4j/Queue.java
@@ -0,0 +1,31 @@
+package net.ollie.sc4j;
+
+import java.util.NoSuchElementException;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+
+/**
+ * A mutable sequence where the head can be operated on.
+ *
+ * @author Ollie
+ * @see java.util.Queue
+ */
+public interface Queue<V>
+        extends Sequence.Mutable<V> {
+
+    boolean offer(V value);
+
+    @CheckForNull
+    V poll();
+
+    @CheckForNull
+    V peek();
+
+    @Nonnull
+    V element() throws NoSuchElementException;
+
+    @Nonnull
+    V remove() throws NoSuchElementException;
+
+}
