f57ac7642901acd47bd16a58c3d97d8217af8d57
Ollie <ollie.robertshaw@gmail.com>
2015-11-17 09:13:56 +0000

    Sequence

--- coljate-api/src/main/java/net/ollie/coljate/lists/Sequence.java
+++ coljate-api/src/main/java/net/ollie/coljate/lists/Sequence.java
@@ -1,5 +1,10 @@
 package net.ollie.coljate.lists;
 
+import java.util.function.Function;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
 import net.ollie.coljate.theory.Container;
 import net.ollie.coljate.theory.Streamable;
 
@@ -7,6 +12,14 @@ import net.ollie.coljate.theory.Streamable;
  *
  * @author Ollie
  */
-public interface Sequence<T> extends Streamable<T>, Container {
+public interface Sequence<@NonNull N, @Nullable T> extends Function<N, T>, Streamable<T>, Container {
+
+    T at(N index);
+
+    @Override
+    @Deprecated
+    default T apply(final N index) {
+        return this.at(index);
+    }
 
 }
