3164454f2caf5c473ea12e78c752fffeee4919c5
Ollie <ollie.robertshaw@gmail.com>
2016-12-11 13:06:17 +0000

    Lazy list

--- src/main/java/net/coljate/list/impl/LazyTransformList.java
+++ src/main/java/net/coljate/list/impl/LazyTransformList.java
@@ -1,9 +1,65 @@
 package net.coljate.list.impl;
 
+import java.util.function.Function;
+
+import net.coljate.list.AbstractList;
+import net.coljate.list.List;
+import net.coljate.list.ListIterator;
+
 /**
  *
  * @author Ollie
  */
-public class LazyTransformList {
+public class LazyTransformList<F, T>
+        extends AbstractList<T> {
+
+    private final List<F> delegate;
+    private final Function<? super F, ? extends T> transform;
+
+    public LazyTransformList(final List<F> delegate, final Function<? super F, ? extends T> transform) {
+        this.delegate = delegate;
+        this.transform = transform;
+    }
+
+    @Override
+    public T first() {
+        return transform.apply(delegate.first());
+    }
+
+    @Override
+    public T last() {
+        return transform.apply(delegate.last());
+    }
+
+    @Override
+    public ListIterator<T> iterator() {
+        return new TransformedListIterator();
+    }
+
+    private final class TransformedListIterator implements ListIterator<T> {
+
+        final ListIterator<F> iterator = delegate.iterator();
+
+        @Override
+        public boolean hasPrevious() {
+            return iterator.hasPrevious();
+        }
+
+        @Override
+        public T previous() {
+            return transform.apply(iterator.previous());
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public T next() {
+            return transform.apply(iterator.next());
+        }
+
+    }
 
 }
