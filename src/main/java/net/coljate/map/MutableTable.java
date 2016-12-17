5bba1eea6f3228dc06fe886b5522f66de9d2e1f1
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2016-12-17 00:11:02 +0000

    Mutable table

---
+++ src/main/java/net/coljate/map/MutableTable.java
@@ -0,0 +1,44 @@
+package net.coljate.map;
+
+import java.util.Objects;
+
+import net.coljate.set.MutableSet;
+
+/**
+ *
+ * @author ollie
+ */
+public interface MutableTable<R, C, V>
+        extends Table<R, C, V>, MutableSet<Cell<R, C, V>> {
+
+    V put(R rowKey, C columnKey, V value);
+
+    @Override
+    default boolean add(final Cell<R, C, V> cell) {
+        return this.add(cell.rowKey(), cell.columnKey(), cell.value());
+    }
+
+    default boolean add(final R rowKey, final C columnKey, final V value) {
+        return Objects.equals(this.get(rowKey, columnKey), value)
+                ? false
+                : this.put(rowKey, columnKey, value) == null;
+    }
+
+    boolean remove(Object rowKey, Object columney, Object value);
+
+    @Override
+    default boolean remove(final Object object) {
+        return object instanceof Cell
+                && this.remove((Cell) object);
+    }
+
+    default boolean remove(final Cell<?, ?, ?> cell) {
+        return this.remove(cell.rowKey(), cell.columnKey(), cell.value());
+    }
+
+    default boolean remove(Object rowKey, Object columnKey) {
+        final Cell<R, C, V> cell = this.cellIfPresent(rowKey, columnKey);
+        return this.remove(cell);
+    }
+
+}
--- src/main/java/net/coljate/map/Table.java
+++ src/main/java/net/coljate/map/Table.java
@@ -1,6 +1,7 @@
 package net.coljate.map;
 
 import net.coljate.set.Set;
+import net.coljate.util.Functions;
 
 /**
  *
@@ -14,7 +15,16 @@ public interface Table<R, C, V> extends Set<Cell<R, C, V>> {
         return this.cellIfPresent(row, column);
     }
 
+    default V get(final R row, final C columnKey) {
+        final Cell<R, C, V> cell = this.cell(row, columnKey);
+        return cell == null ? null : cell.value();
+    }
+
     @SuppressWarnings("unchecked")
     Cell<R, C, V> cellIfPresent(Object row, Object column);
+
+    default V getIfPresent(final Object row, final Object columnKey) {
+        return Functions.ifNonNull(this.cellIfPresent(row, columnKey), Cell::value);
+    }
 
 }
