76a96a58ec541c56a9d16d9a7789e860c16e8efa
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2016-12-18 19:51:35 +0000

    Abstract counter

--- src/main/java/net/coljate/counter/AbstractCounter.java
+++ src/main/java/net/coljate/counter/AbstractCounter.java
@@ -1,6 +1,7 @@
 package net.coljate.counter;
 
 import net.coljate.collection.AbstractCollection;
+import net.coljate.util.Equality;
 
 /**
  *
@@ -11,7 +12,13 @@ public abstract class AbstractCounter<T>
 
     @Override
     protected boolean equals(AbstractCollection<?> that) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return that instanceof AbstractCounter
+                && this.equals((AbstractCounter<?>) that);
+    }
+
+    protected boolean equals(final AbstractCounter<?> that) {
+        return this.getClass() == that.getClass()
+                && Equality.unorderedEquals(this, that);
     }
 
 }
