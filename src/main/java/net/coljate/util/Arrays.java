a8a478dd0c88f3fe095e678a092ac2a43d78c960
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2016-12-21 21:36:25 +0000

    Transform values

--- src/main/java/net/coljate/util/Arrays.java
+++ src/main/java/net/coljate/util/Arrays.java
@@ -1,6 +1,5 @@
 package net.coljate.util;
 
-import java.util.Comparator;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -74,6 +73,21 @@ public final class Arrays {
             into[i] = transform.apply(from[i]);
         }
         return into;
+    }
+
+    public static <T> T[] copy(final T[] source) {
+        return java.util.Arrays.copyOf(source, source.length);
+    }
+
+    public static Object[][] copy(final Object[][] source) {
+        if (source.length == 0) {
+            return source;
+        }
+        final Object[][] target = new Object[source.length][source[0].length];
+        for (int i = 0; i < source.length; i++) {
+            target[i] = copy(source[i]);
+        }
+        return target;
     }
 
 }
