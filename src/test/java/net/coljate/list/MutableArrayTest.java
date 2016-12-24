e16e87dabad24b59a56fdb5a20f26e9292482754
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2016-12-24 10:49:01 +0000

    Resize tests

--- src/test/java/net/coljate/list/MutableArrayTest.java
+++ src/test/java/net/coljate/list/MutableArrayTest.java
@@ -1,5 +1,9 @@
 package net.coljate.list;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.jupiter.api.Test;
+
 /**
  *
  * @author ollie
@@ -10,6 +14,16 @@ public interface MutableArrayTest<T> extends ArrayTest<T>, MutableListTest<T> {
     MutableArray<T> createTestCollection();
 
     interface ZeroElementTests<T> extends MutableArrayTest<T>, ArrayTest.ZeroElementTests<T>, MutableListTest.ZeroElementTests<T> {
+
+        @Test
+        default void testResize() {
+            final MutableArray<T> array = this.createTestCollection();
+            assertThat(array.length(), is(0));
+            array.resize(1);
+            assertThat(array.length(), is(1));
+            array.resize(1);
+            assertThat(array.length(), is(1));
+        }
 
     }
 
