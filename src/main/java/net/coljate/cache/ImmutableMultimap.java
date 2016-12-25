1eed1330ab1ea64f9962714010eeabddb82192f2
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2016-12-25 10:03:20 +0000

    Immutable multimap

---
+++ src/main/java/net/coljate/cache/ImmutableMultimap.java
@@ -0,0 +1,39 @@
+package net.coljate.cache;
+
+import net.coljate.collection.Collection;
+import net.coljate.collection.ImmutableCollection;
+import net.coljate.map.Entry;
+import net.coljate.map.ImmutableEntry;
+import net.coljate.map.ImmutableMap;
+import net.coljate.util.Functions;
+import net.coljate.util.iterator.UnmodifiableCovariantIterator;
+
+/**
+ *
+ * @author ollie
+ */
+public interface ImmutableMultimap<K, V>
+        extends Multimap<K, V>, ImmutableMap<K, Collection<V>> {
+
+    @Override
+    ImmutableMultimapEntry<K, V, ? extends ImmutableCollection<V>> getEntry(Object key);
+
+    @Override
+    default ImmutableCollection<V> get(Object key) {
+        return Functions.ifNonNull(this.getEntry(key), ImmutableMultimapEntry::value);
+    }
+
+    @Override
+    UnmodifiableCovariantIterator<Entry<K, Collection<V>>, ?> iterator();
+
+    @Override
+    @Deprecated
+    default ImmutableMultimap<K, V> immutableCopy() {
+        return this;
+    }
+
+    interface ImmutableMultimapEntry<K, V, C extends ImmutableCollection<V>> extends MultimapEntry<K, V, C>, ImmutableEntry<K, Collection<V>> {
+
+    }
+
+}
--- src/main/java/net/coljate/cache/Multimap.java
+++ src/main/java/net/coljate/cache/Multimap.java
@@ -1,11 +1,29 @@
 package net.coljate.cache;
 
 import net.coljate.collection.Collection;
+import net.coljate.map.Entry;
+import net.coljate.util.iterator.CovariantIterator;
 
 /**
  *
  * @author ollie
  */
 public interface Multimap<K, V> extends Cache<K, Collection<V>> {
+
+    @Override
+    MultimapEntry<K, V, ? extends Collection<V>> getEntry(Object key);
+
+    @Override
+    CovariantIterator<Entry<K, Collection<V>>, ?> iterator();
+
+    @Override
+    ImmutableMultimap<K, V> immutableCopy();
+
+    interface MultimapEntry<K, V, C extends Collection<V>> extends Entry<K, Collection<V>> {
+
+        @Override
+        C value();
+
+    }
 
 }
