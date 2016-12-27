965e8dd2e5cb0bf30d87773e0998fdaf24ff7710
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2016-12-27 18:03:11 +0000

    Graphs

---
+++ src/main/java/net/coljate/graph/UndirectedGraph.java
@@ -0,0 +1,24 @@
+package net.coljate.graph;
+
+import net.coljate.util.iterator.CovariantIterator;
+
+/**
+ * @author ollie
+ */
+public interface UndirectedGraph<V, E> extends Graph<V, E> {
+
+    @Override
+    CovariantIterator<Relationship<V, E>, ? extends UndirectedRelationship<V, E>> iterator();
+
+    interface UndirectedRelationship<V, E> extends Relationship<V, E> {
+
+        @Override
+        default boolean isBetween(final Object fromVertex, final Object toVertex) {
+            return this.isFrom(fromVertex)
+                    ? this.isTo(toVertex)
+                    : this.isTo(fromVertex) && this.isFrom(toVertex);
+        }
+
+    }
+
+}
