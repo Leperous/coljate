5fa6f2f989acedb0f62f8f8472f095f0d3dbd929
Ollie <ollie.robertshaw@gmail.com>
2017-01-01 13:06:07 +0000

    Memory complexity

--- src/main/java/net/coljate/Container.java
+++ src/main/java/net/coljate/Container.java
@@ -2,6 +2,7 @@ package net.coljate;
 
 import net.coljate.collection.Collection;
 import net.coljate.util.complexity.Complexity;
+import net.coljate.util.complexity.MemoryComplexity;
 import net.coljate.util.complexity.TimeComplexity;
 
 /**
@@ -13,18 +14,18 @@ import net.coljate.util.complexity.TimeComplexity;
 public interface Container {
 
     /**
-     *
      * @param object
-     * @return true if this container contains the given object.
+     * @return true if this container contains the given object, according to its {@link Object#equals equals} method.
      */
     @TimeComplexity(bestCase = Complexity.CONSTANT, worstCase = Complexity.LINEAR)
+    @MemoryComplexity(Complexity.CONSTANT)
     boolean contains(Object object);
 
     /**
-     *
      * @return true if there are no objects inside this container.
      */
     @TimeComplexity(Complexity.CONSTANT)
+    @MemoryComplexity(Complexity.CONSTANT)
     boolean isEmpty();
 
 }
