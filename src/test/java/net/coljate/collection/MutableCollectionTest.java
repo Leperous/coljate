0b1094491d3ba938d18a547e9ee9f51f99bb180f
Ollie <ollie.robertshaw@gmail.com>
2017-01-02 10:43:28 +0000

    Test clear

--- src/test/java/net/coljate/collection/MutableCollectionTest.java
+++ src/test/java/net/coljate/collection/MutableCollectionTest.java
@@ -1,8 +1,12 @@
 package net.coljate.collection;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import net.coljate.TestObjectCreator;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -24,6 +28,20 @@ public interface MutableCollectionTest<T> extends CollectionTest<T>, TestObjectC
         @Test
         default void testRemoveFirst() {
             assertFalse(this.createTestCollection().removeFirst(this.createTestObject()));
+        }
+
+    }
+
+    interface OneElementTests<T> extends MutableCollectionTest<T>, CollectionTest.OneElementTests<T> {
+
+        @Test
+        default void testClear() {
+            final MutableCollection<T> collection = this.createTestCollection();
+            final T element = this.getCollectionElement();
+            assertTrue(collection.contains(element));
+            collection.clear();
+            assertFalse(collection.contains(element));
+            assertThat(collection.count(), is(0));
         }
 
     }
