cc8e401b21093db7846eeb1743e214d2ce617bc6
Ollie <ollie.robertshaw@gmail.com>
2017-01-02 11:12:25 +0000

    Concurrent linked deque does not permit null elements

--- src/main/java/net/coljate/list/impl/WrappedConcurrentLinkedDeque.java
+++ src/main/java/net/coljate/list/impl/WrappedConcurrentLinkedDeque.java
@@ -1,15 +1,12 @@
 package net.coljate.list.impl;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.ListIterator;
 import java.util.OptionalInt;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+
 import net.coljate.list.ConcurrentList;
 import net.coljate.util.Functions;
-
-import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
  *
@@ -35,66 +32,16 @@ public class WrappedConcurrentLinkedDeque<@NonNull T>
     }
 
     @Override
-    public WrappedConcurrentLinkedDeque<T> mutableCopy() {
-        return new WrappedConcurrentLinkedDeque<>(this.mutableJavaCopy(i -> new ConcurrentLinkedDeque<>()));
+    public boolean add(final T element) {
+        if (element == null) {
+            return false;
+        }
+        return super.add(element);
     }
 
-    public static class ConcurrentLinkedDequeListBridge<T>
-            extends java.util.concurrent.ConcurrentLinkedDeque<T>
-            implements java.util.List<T> {
-
-        private static final long serialVersionUID = 1L;
-
-        @Override
-        public boolean addAll(int index, Collection<? extends T> c) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public T get(final int index) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public T set(final int index, final T element) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public void add(int index, T element) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public T remove(int index) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public int indexOf(Object o) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public int lastIndexOf(Object o) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public ListIterator<T> listIterator() {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public ListIterator<T> listIterator(int index) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public List<T> subList(int fromIndex, int toIndex) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
+    @Override
+    public WrappedConcurrentLinkedDeque<T> mutableCopy() {
+        return new WrappedConcurrentLinkedDeque<>(this.mutableJavaCopy(i -> new ConcurrentLinkedDeque<>()));
     }
 
 }
