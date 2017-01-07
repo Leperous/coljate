8ac4bf6e9f4c668623da4b993069a0afb32a8e7d
Ollie <ollie.robertshaw@gmail.com>
2017-01-07 16:44:54 +0000

    Guava graph

---
+++ guava20/src/main/java/net/coljate/graph/impl/GuavaDirectedGraph.java
@@ -0,0 +1,80 @@
+package net.coljate.graph.impl;
+
+import com.google.common.graph.EndpointPair;
+
+import net.coljate.graph.AbstractGraph;
+import net.coljate.graph.AbstractRelationship;
+import net.coljate.graph.DirectedGraph;
+import net.coljate.graph.DirectedRelationship;
+import net.coljate.graph.ImmutableDirectedGraph;
+import net.coljate.graph.Relationship;
+import net.coljate.set.Set;
+import net.coljate.util.iterator.CovariantIterator;
+import net.coljate.util.iterator.Iterators;
+
+/**
+ *
+ * @author Ollie
+ */
+public class GuavaDirectedGraph<V, E>
+        extends AbstractGraph<V, Set<E>>
+        implements DirectedGraph<V, Set<E>> {
+
+    private final com.google.common.graph.Network<V, E> graph;
+    private Set<V> vertices;
+
+    protected GuavaDirectedGraph(final com.google.common.graph.Network<V, E> graph) {
+        if (!graph.isDirected()) {
+            throw new IllegalArgumentException("Not a directed graph: " + graph);
+        }
+        this.graph = graph;
+    }
+
+    @Override
+    public Set<V> vertices() {
+        return vertices == null
+                ? vertices = Set.viewOf(graph.nodes())
+                : vertices;
+    }
+
+    @Override
+    public CovariantIterator<Relationship<V, Set<E>>, ? extends DirectedRelationship<V, Set<E>>> iterator() {
+        return Iterators.transform(graph.asGraph().edges().iterator(), GuavaRelationship::new);
+    }
+
+    @Override
+    public ImmutableDirectedGraph<V, Set<E>> immutableCopy() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public class GuavaRelationship extends AbstractRelationship<V, Set<E>> implements DirectedRelationship<V, Set<E>> {
+
+        private final EndpointPair<V> endpoints;
+
+        protected GuavaRelationship(final EndpointPair<V> endpoints) {
+            this.endpoints = endpoints;
+        }
+
+        @Override
+        public V from() {
+            return endpoints.nodeU();
+        }
+
+        @Override
+        public V to() {
+            return endpoints.nodeV();
+        }
+
+        @Override
+        public Set<E> edge() {
+            return Set.viewOf(graph.edgesConnecting(endpoints.nodeU(), endpoints.nodeV()));
+        }
+
+        @Override
+        protected boolean equals(final Relationship<?, ?> that) {
+            throw new UnsupportedOperationException(); //TODO
+        }
+
+    }
+
+}
