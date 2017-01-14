5c910e0f70a892817ea6052941833e68acfa6aaf
Ollie <ollie.robertshaw@gmail.com>
2017-01-14 11:48:34 +0000

    Value graph

--- api/src/main/java/net/coljate/graph/impl/MappedValueGraph.java
+++ api/src/main/java/net/coljate/graph/impl/MappedValueGraph.java
@@ -1,13 +1,8 @@
 package net.coljate.graph.impl;
 
-import java.util.Iterator;
 import java.util.Objects;
 
-import net.coljate.graph.AbstractGraph;
 import net.coljate.graph.Graph;
-import net.coljate.graph.ImmutableGraph;
-import net.coljate.graph.MutableGraph;
-import net.coljate.graph.Relationship;
 import net.coljate.graph.ValueGraph;
 import net.coljate.map.Entry;
 import net.coljate.map.Map;
@@ -17,20 +12,14 @@ import net.coljate.map.Map;
  * @author Ollie
  */
 public class MappedValueGraph<K, V, E>
-        extends AbstractGraph<K, E>
+        extends DelegatedGraph<K, E>
         implements ValueGraph<K, V, E> {
 
-    private final Graph<K, E> graph;
     private final Map<K, V> vertexValues;
 
-    public MappedValueGraph(final Graph<K, E> delegate, final Map<K, V> vertexValues) {
-        this.graph = Objects.requireNonNull(delegate);
+    public MappedValueGraph(final Graph<K, E> graph, final Map<K, V> vertexValues) {
+        super(graph);
         this.vertexValues = Objects.requireNonNull(vertexValues);
-    }
-
-    @Override
-    public Iterator<Relationship<K, E>> iterator() {
-        return graph.iterator();
     }
 
     @Override
@@ -46,16 +35,6 @@ public class MappedValueGraph<K, V, E>
     @Override
     public Entry<K, V> getVertex(final Object vertexKey) {
         return vertexValues.getEntry(vertexKey);
-    }
-
-    @Override
-    public MutableGraph<K, E> mutableCopy() {
-        return graph.mutableCopy();
-    }
-
-    @Override
-    public ImmutableGraph<K, E> immutableCopy() {
-        return graph.immutableCopy();
     }
 
 }
