fa1180b460500bbe7eae73cad63425238c2405bb
Ollie Robertshaw <ollie.robertshaw@gmail.com>
2017-09-01 20:44:22 +0100

    int counter

---
+++ api/src/main/java/net/coljate/counter/primitive/IntegerCounter.java
@@ -0,0 +1,33 @@
+package net.coljate.counter.primitive;
+
+import net.coljate.collection.primitive.IntegerCollection;
+import net.coljate.counter.Counter;
+
+/**
+ *
+ * @author ollie
+ */
+public interface IntegerCounter extends IntegerCollection, Counter<Integer> {
+
+    int count(int value);
+
+    @Override
+    default boolean contains(int i) {
+        return this.count(i) > 0;
+    }
+
+    @Override
+    @Deprecated
+    default int count(final Object element) {
+        return element instanceof Integer
+                ? this.count(((Integer) element).intValue())
+                : 0;
+    }
+
+    @Override
+    @Deprecated
+    default boolean contains(Object element) {
+        return Counter.super.contains(element);
+    }
+
+}
