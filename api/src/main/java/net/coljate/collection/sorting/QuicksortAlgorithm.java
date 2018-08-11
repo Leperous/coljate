144781541676cac4c606ae0d49d6ab97cd739719
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2018-08-11 12:16:40 +0100

    Quicksort

--- api/src/main/java/net/coljate/collection/sorting/QuicksortAlgorithm.java
+++ api/src/main/java/net/coljate/collection/sorting/QuicksortAlgorithm.java
@@ -3,10 +3,17 @@ package net.coljate.collection.sorting;
 import java.util.Comparator;
 
 /**
- * Select a partition index, and divide and sort the rest of the array into two parts, one where the values are are
- * less than the value at the partition index (the pivot) and the other where they are greater than the pivot.
+ * Select a `random' partition index, and re-order the array so thatelements less than the value at the partition index
+ * (the `pivot') precede it, and elements greater than the pivot succeed it.
+ * By performing this recursively on smaller subarrays the original array is eventually fully sorted.
+ * <p>
+ * Uses Lomuto partition scheme.
  * <p>
  * For example suppose you have an array {@code [3, 2, 5, 1, 4]}.
+ * Choose the left-most element {@code 3} as the initial pivot.
+ * For each element in the array, if it is less than the right-most element, swap it with the pivot and increment the
+ * partition index;
+ * thus {@code 3<4 => pivot=2}; {@code 2<4 => pivot=5}; {@code 1<4 => pivot=}
  */
 class QuicksortAlgorithm implements SortingAlgorithm {
 
